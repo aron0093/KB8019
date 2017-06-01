71987de5f94bdb3a51bd482b08ad17eed0467602
aron0093 <aron0093@gmail.com>
2017-06-01 15:35:16 +0200

    Updated script to count ORFs in each score class

--- Codes/toCountStuff.py
+++ Codes/toCountStuff.py
@@ -1,13 +1,42 @@
 # To count stuff in a FASTA file
+
 import sys
 
 f = open(sys.argv[1],'r') 
-t = list()
+
+#count = 0
+
+count_1 = 0
+count_2 = 0
+count_3 = 0
 
 for i in f.readlines():
-	if i [0] == '>':
-		t.append(i)
 
+    if i [0] == '>':
+#        count += 1
+        
+        words = i.partition('_')
+        
+        score = int(words[2][8])
+        
+        if score == 1:
+        
+            count_1 += 1
+        
+        elif score == 2:
+        
+            count_2 += 1
+            
+        elif score == 3:
+        
+            count_3 += 1
+            
+        else:
+        
+            print('Error')
 f.close()
-print(len(t))
+#print(count)
 
+print(count_1)
+print(count_2)
+print(count_3)
